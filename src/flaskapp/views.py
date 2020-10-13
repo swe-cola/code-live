9ccedd45ab3ee472b81ed1f0fc5a29c97a37dad3
Semin Park <SPark9625@gmail.com>
2020-10-13 11:30:11 +0900

    Demo editor using yorkie

--- src/flaskapp/views.py
+++ src/flaskapp/views.py
@@ -1,7 +1,8 @@
+import os
 from flaskapp import app
 from flask import render_template
 
 
 @app.route('/')
 def route_index():
-    return render_template("index.html")
+    return render_template("index.html", API_URL=os.environ['YORKIE_AGENT_URL'])
