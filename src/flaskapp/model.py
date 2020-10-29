60ded868ea03231a120698216509f4e04551df90
Semin Park <SPark9625@gmail.com>
2020-10-29 22:38:04 +0900

    Add field nickname to User model

--- src/flaskapp/model.py
+++ src/flaskapp/model.py
@@ -4,3 +4,4 @@ class User(me.Document):
     lastAccess = me.DateTimeField(required=True)
     documents = me.ListField()
     maxDocuments = me.IntField(default=10)
+    nickname = me.StringField()
