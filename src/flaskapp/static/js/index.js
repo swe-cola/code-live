9c819df84d7bc60c41cf930ee575626a6bf38fe1
Unknown <cookkyh99@skku.edu>
2020-10-31 14:36:11 +0900

    console dark/light mode

--- src/flaskapp/static/js/index.js
+++ src/flaskapp/static/js/index.js
@@ -230,3 +230,14 @@ function colortheme(element){
 if (window.matchMedia("(prefers-color-scheme: light)").matches) {
     document.documentElement.classList.add("light");
 }
+
+$('#darkModeSwitch input[type=checkbox]').on('click', function() {
+  const cm = $('.CodeMirror').get(0).CodeMirror;
+  if ($(this).prop('checked')) {
+    // Dark mode
+    cm.setOption('theme', 'material');
+  } else {
+    // Light mode
+    cm.setOption('theme', 'solarized');
+  }
+});
