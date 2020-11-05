acaf0a7340dfca2beb8605247049cbfae55135f0
Semin Park <SPark9625@gmail.com>
2020-11-05 15:22:33 +0900

    Implement naive GC logic

--- src/flaskapp/garbage_collect.py
+++ src/flaskapp/garbage_collect.py
@@ -1,17 +1,59 @@
 import multiprocessing as mp
 import os
+import time
+from datetime import datetime, timedelta
+from pymongo import MongoClient
 
-def remove_inactive_documents():
-    '''Naive garbage collection implementation.
-    This wakes up every hour and cleans all documents
-    that are inactive for the last 24 hours.
+
+def seconds_till_next_hour():
+    '''Returns the seconds left until the next hour.
+    If now is 14H:25M:17S, this returns the seconds left until 15H:00M:00S
     '''
-    pass
+    now = datetime.utcnow()
+    target = datetime(now.year, now.month, now.day, now.hour) + timedelta(hours=1)
+    delta = target - now
+    return delta.seconds
+
+def remove_inactive_documents(client):
+    '''Naive garbage collection implementation.
+    This cleans all documents that have been inactive
+    for the last 24 hours.
+    '''
+    cl = client['code-live']
+    ym = client['yorkie-meta']
+
+    now = datetime.utcnow()
+    criteria = now - timedelta(hours=24)
+    print(f'Now is {now}. Will run GC for inactive elements since {criteria}')
+
+    # handle code-live
+    x = cl.user.delete_many({'lastAccess': {'$lt': criteria}})
+    print(f'Deleted {x.deleted_count} users from the code-live database')
+
+    # handle yorkie-meta
+    x = ym.clients.delete_many({'updated_at': {'$lt': criteria}})
+    print(f'Deleted {x.deleted_count} clients from the yorkie-meta database')
+    for doc in ym.documents.find({'updated_at': {'$lt': criteria}}):
+        docid = doc['_id']
+        print(f'Processing document {docid}')
+        x = ym.changes.delete_many({'doc_id': docid})
+        print(f'  Deleted {x.deleted_count} changes from the yorkie-meta database')
+        x = ym.snapshots.delete_many({'doc_id': docid})
+        print(f'  Deleted {x.deleted_count} snapshots from the yorkie-meta database')
+        x = ym.syncedseqs.delete_many({'doc_id': docid})
+        print(f'  Deleted {x.deleted_count} syncedseqs from the yorkie-meta database')
+    x = ym.documents.delete_many({'updated_at': {'$lt': criteria}})
+    print(f'Deleted {x.deleted_count} documents from the yorkie-meta database')
 
 def gc_inactive_documents():
+    client = MongoClient(
+        host=os.environ['MONGO_USER_HOST'],
+        port=int(os.environ['MONGO_USER_PORT']),
+    )
     while True:
-        time.sleep(3600)
-        remove_inactive_documents()
+        remove_inactive_documents(client)
+        seconds = seconds_till_next_hour()
+        time.sleep(seconds)
 
 
 def run_gc():
