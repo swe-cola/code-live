318b24cab3c2759fe2a0e9e902b04ac97c681d80
rrr-dw <71978264+rrr-dw@users.noreply.github.com>
2020-11-10 03:49:22 +0900

    support some languages that defines its own autocomplete (ex. javascript)

--- src/flaskapp/static/js/index.js
+++ src/flaskapp/static/js/index.js
@@ -251,10 +251,16 @@ async function main() {
 
         // 06. setup auto-completion.
         function merged_hint(cm, options) {
-            var words = cm.getHelper(cm.getCursor(),"hintWords");
+            //var words = cm.getHelper(cm.getCursor(),"hintWords");
             var result_hint = CodeMirror.hint.anyword(cm, options); // hint based on current snippet contents
-            var lang_hint = CodeMirror.hint.fromList(cm, {words: words}); // mode dependent hint. Return undefined on no candidates.
-            if(lang_hint!=undefined){
+            var resolved_hint = CodeMirror.hint.auto.resolve(cm,cm.getCursor());
+            var lang_hint;// mode dependent hint. Return undefined on no candidates.
+            if(resolved_hint.async)
+                resolved_hint(cm,function(r){lang_hint=r;},options);
+            else
+                lang_hint = resolved_hint(cm,options);
+
+            if(lang_hint){
                 var unique_items = new Set([ ...result_hint.list , ...lang_hint.list ]);
                 result_hint.list = [...unique_items]; // merge two hints
             }
