ec8e3f162565ab35ffa63152cebac0c141dc7b41
Unknown <cookkyh99@skku.edu>
2020-11-10 09:03:13 +0900

    세션 사용하도록 변경

--- src/flaskapp/static/js/login.js
+++ src/flaskapp/static/js/login.js
@@ -1,76 +1,80 @@
 Kakao.init( "3634d739399c9436fa103280f8e6cb34" );
 
-$( document ).ready(function() {
-    let cookie = document.cookie;
-    if (cookie.includes("email")){
-        cookie = parseCookieString();
-        console.log(cookie);
-        loginUser(cookie["nickname"] , cookie["email"], cookie["thumbnail"]);
-        return;
-    }
-    Kakao.Auth.createLoginButton({
-        container : "#login_btn",
-        success : function( authObj ) {
+
+$('#login_btn').on('click', function(){
+    Kakao.Auth.login({
+        scope: 'account_email,profile',
+        success: function(response) {
+            let access_token = response["access_token"];
+
             Kakao.API.request({
-                url : "/v2/user/me"
-                , success : function( res ) {
-                    let nickname = res.properties.nickname;
-                    let email = res.kakao_account.email;
-                    let thumbnail = res.properties.thumbnail_image;
+                url: '/v2/user/me',
+                success: function(response) {
+                    let profile = response["kakao_account"];
+                    let email = profile["email"];
+                    let nickname = profile["profile"]["nickname"];
+                    let thumbnail = profile["profile"]["thumbnail_image_url"];
+
+                    sessionStorage.setItem("nickname", nickname);
+                    sessionStorage.setItem("email", email);
+                    sessionStorage.setItem("thumbnail", thumbnail);
+
                     loginUser(nickname, email, thumbnail);
-                }, fail : function( error ) {
-                    alert( JSON.stringify( error ) );
+                },
+                fail: function(error) {
+                    console.log(error);
                 }
             });
-        }
-        , fail : function( error ) {
-            alert( JSON.stringify( error ));
+        },
+        fail: function(error) {
+            console.log(error);
         }
     });
-
-    $("#kakao-login-btn").attr("src", "");
-    $("#login_btn").html("Login");
 });
 
-function loginUser(nickname, email, thumbnail){
-    document.getElementById( "user_profile" ).src = thumbnail;
-    $("#user_profile").removeClass("dis_none");
-    $("#login_btn").addClass("dis_none");
-    $("#logout_btn").removeClass("dis_none");
-
-    if (email !== "") {
-        document.cookie = `nickname=${nickname}`;
-        document.cookie = `email=${email}`;
-        document.cookie = `thumbnail=${thumbnail}`;
-    }
-}
-
-function parseCookieString(){
-    let cookie_obj = {};
-    let semi_parsed = document.cookie.split(';');
-    $.each(semi_parsed, function(index, item){
-        let stripped = $.trim(item);
-        let value_dict = stripped.split("=");
-        let key = value_dict[0];
-        let value = value_dict[1];
-        cookie_obj[key] = value;
-    });
-    return cookie_obj;
-}
-
 $('#logout_btn').on('click', function() {
-    let cookie = parseCookieString();
-    delete cookie["nickname"];
-    delete cookie["email"];
-    delete cookie["thumbnail"];
-    document.getElementById( "user_profile" ).src = "";
     $("#user_profile").addClass("dis_none");
+    document.getElementById( "user_profile" ).src = "";
+    console.log(sessionStorage.length);
+    sessionStorage.removeItem("nickname");
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("thumbnail");
+    console.log(sessionStorage.length);
     $("#login_btn").removeClass("dis_none");
     $("#logout_btn").addClass("dis_none");
 
-    Kakao.Auth.logout(() => {
-        this.setState({
-            isLogin: false
-        })
-    });
+    Kakao.Auth.logout();
 });
+
+//     $("#login_btn").removeClass("dis_none");
+//     $("#logout_btn").addClass("dis_none");
+//
+//     if (!Kakao.Auth.getAccessToken()) {
+//       console.log('Not logged in.');
+//       return;
+//     }
+//     Kakao.Auth.logout(function() {
+//       console.log(Kakao.Auth.getAccessToken());
+//     });
+// });
+
+$( document ).ready(function() {
+    let email=null, nickname=null, thumbnail=null;
+    for(let i=0; i<sessionStorage.length; i++){
+        let key = sessionStorage.key(i);
+        if(key=="email") email = sessionStorage.getItem(key);
+        else if(key=="nickname") nicknamenickname = sessionStorage.getItem(key);
+        else if(key=="thumbnail") thumbnail = sessionStorage.getItem(key);
+    }
+    if(email !== null){
+        loginUser(email, nickname, thumbnail);
+    }
+});
+
+
+function loginUser(nickname, email, thumbnail){
+    $("#user_profile").attr("src", thumbnail);
+    $("#user_profile").removeClass("dis_none");
+    $("#login_btn").addClass("dis_none");
+    $("#logout_btn").removeClass("dis_none");
+}
