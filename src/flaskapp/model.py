eec681e24280c9a794c2c7e219295e65836d7794
Semin Park <SPark9625@gmail.com>
2020-11-25 02:42:35 +0900

    Add Document model

--- src/flaskapp/model.py
+++ src/flaskapp/model.py
@@ -6,3 +6,11 @@ class User(me.Document):
     maxDocuments = me.IntField(default=10)
     nickname = me.StringField()
     kakaoid = me.StringField()
+
+
+class Document(me.Document):
+    lastAccess = me.DateTimeField(required=True)
+    document_id = me.StringField(required=True)
+    owner = me.StringField(required=True)
+    clients = me.DictField(default={})
+    login = me.BooleanField(default=False)
