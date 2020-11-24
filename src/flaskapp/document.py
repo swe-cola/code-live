93d5e53642799eee162365000d824a4db79d8278
Semin Park <SPark9625@gmail.com>
2020-11-25 02:42:16 +0900

    Refactor document.py

--- src/flaskapp/document.py
+++ src/flaskapp/document.py
@@ -1,118 +1,81 @@
+from datetime import datetime
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from flaskapp.model import Document
+from flaskapp.user import get_user
 import os
 import random
 import string
 
 
-def create_db_client():
-    '''create_db_client creates MongoClient once and then caches it.
-    '''
-    client = MongoClient(
-        host=os.environ['MONGO_USER_HOST'],
-        port=int(os.environ['MONGO_USER_PORT']),
-    )
-    def get_db():
-        return client
-    return get_db
+def exists(doc_id):
+    if Document.objects(document_id=doc_id):
+        return True
+    return False
 
-get_db = create_db_client()
+
+def get_document(doc_id):
+    if not exists(doc_id):
+        return None
+    return Document.objects(document_id=doc_id).first()
 
 
 def save_document_info(doc_id, owner, login):
-    client = get_db()
-    db = client["code-live"]
-    col = db["documents"]
-    doc_info = {"document_id": doc_id, "owner": owner, "login": login}
-
-    col.insert_one(doc_info)
-
-
-def update_document_clients(doc_id, clientID):
-    client = get_db()
-    db = client["code-live"]
-    col = db["documents"]
-
-    doc_query = {"document_id": doc_id}
-    cursor = col.find(doc_query)
-    result_doc = cursor.next()
-
-    nickname = route_generate_nickname(doc_id, clientID)
-    if 'clients' in result_doc and len(result_doc['clients']) != 0:
-        while nickname in result_doc['clients'].values():
-            nickname = route_generate_nickname(doc_id, clientID)
-
-    # {codelive_id: nickname}
-    if "clients" in result_doc:
-        client_dict = result_doc["clients"]
-        client_dict[clientID] = nickname
+    doc = get_document(doc_id)
+    if doc:
+        doc.lastAccess = datetime.utcnow()
+        doc.owner = owner
+        doc.login = login
     else:
-        client_dict = {clientID: nickname}
-    newvalues = {"$set": {"clients": client_dict}}
-    col.update_one(doc_query, newvalues)
+        doc = Document(lastAccess=datetime.utcnow(), document_id=doc_id, owner=owner, login=login)
+    doc.save()
 
-    result_dict = {}
-    for key, value in client_dict.items():
-        result_dict[key] = value
 
-    return result_dict
+def update_document_clients(doc_id, client_id):
+    if not exists(doc_id):
+        raise ValueError('Document does not exist')
+
+    doc = get_document(doc_id)
+    clients = doc.clients
+
+    nickname = clients.get(client_id)
+    if nickname is None:
+        taken = set(doc['clients'].values())
+        nickname = generate_nickname(ignore=taken)
+        clients[client_id] = nickname
+        doc.save()
+    return clients.copy()
 
 
 def update_document_login(owner):
-    client = get_db()
-    db = client["code-live"]
-    col = db["documents"]
-
-    owner_query = {"owner": owner}
-    newvalues = {"$set": {"login": True}}
-
-    col.update_one(owner_query, newvalues)
+    Document.objects(owner=owner).update(set__login=True)
 
 
 def get_document_peers(doc_id):
-    client = get_db()
-    db = client["code-live"]
-    col = db["documents"]
-    user = db["user"]
+    if not exists(doc_id):
+        raise ValueError('Document does not exist')
 
-    doc_query = {"document_id": doc_id}
-    result = list(col.find(doc_query))
+    doc = get_document(doc_id)
+    clients = doc.clients
 
-    result_doc = result[0]
-    clients = result_doc['clients']
-
-    mapping_dict = {}
-
-    for client_key, client_name in clients.items():
-        user_query = {"_id": ObjectId(client_key)}
-        result = list(user.find(user_query))
-        if len(result) != 0 and 'kakaoid' in result[0]:
-            mapping_dict[client_key] = result[0]['kakaoid']
-            continue
-        mapping_dict[client_key] = client_name
-
-    return mapping_dict
+    peers = {}
+    for client_id, client_name in clients.items():
+        user = get_user(client_id)
+        if 'kakaoid' in user:
+            client_name = user.kakaoid
+        peers[client_id] = client_name
+    return peers
 
 
-def delete_document_peers(docid, user_cookie):
-    client = get_db()
-    db = client["code-live"]
-    col = db["documents"]
+def delete_document_peers(doc_id, user_id):
+    if not exists(doc_id):
+        return
 
-    doc_query = {"document_id": docid}
-    result = list(col.find(doc_query))
-
-    result_doc = result[0]
-    clients = result_doc['clients']
-
-    copied_clients = clients.copy()
-
-    for key, value in clients.items():
-        if key == user_cookie:
-            del copied_clients[key]
-
-    newvalues = {"$set": {"clients": copied_clients}}
-    col.update_one(doc_query, newvalues)
+    doc = get_document(doc_id)
+    clients = doc.clients
+    if user_id in clients:
+        clients.pop(user_id)
+    doc.save()
 
 
 def create_doc_id(length=6):
@@ -122,7 +85,7 @@ def create_doc_id(length=6):
     return document_id
 
 
-def route_generate_nickname(docid, clientid):
+def generate_nickname(ignore=None):
     adjectives = ['Adorable', 'Ambitious', 'Angry', 'Attractive', 'Beautiful', 'Big', 'Bored', 'Brave', 'Calm',
                   'Chubby', 'Clean', 'Dazzling', 'Delightful', 'Elegant', 'Fancy', 'Friendly', 'Gentle', 'Glamorous',
                   'Gorgeous', 'Handsome', 'Happy', 'Lazy', 'Muscular', 'Mysterious', 'Nervous', 'Nice', 'Polite',
@@ -142,8 +105,26 @@ def route_generate_nickname(docid, clientid):
         'Wolverine', 'Wombat'
     ]
 
-    key = (docid, clientid)
-    adjective = adjectives[hash(key) % len(adjectives)]
-    animal = animals[hash(key) % len(animals)]
+    def __generate_nickname():
+        adjective = random.choice(adjectives)
+        animal = random.choice(animals)
 
-    return adjective + " " + animal
+        return adjective + " " + animal
+
+    nickname = __generate_nickname()
+    if ignore is not None:
+        while nickname in ignore:
+            nickname = __generate_nickname()
+    return nickname
+
+
+def get_nickname(doc_id, client_id):
+    doc = get_document(doc_id)
+    clients = doc.clients
+    nickname = clients.get(client_id)
+    if nickname is None:
+        taken = set(doc['clients'].values())
+        nickname = generate_nickname(ignore=taken)
+        clients[client_id] = nickname
+        doc.save()
+    return nickname
