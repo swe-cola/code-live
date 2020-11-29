1c0c6f3d729b0b17d36145974a0a37c4d223a8a8
Semin Park <SPark9625@gmail.com>
2020-11-29 22:22:18 +0900

    Connect to mongodb only when needed

--- src/flaskapp/__init__.py
+++ src/flaskapp/__init__.py
@@ -10,6 +10,7 @@ app.config['MONGODB_SETTINGS'] = {
     'db': 'code-live',
     'host': os.environ['MONGO_USER_HOST'],
     'port': int(os.environ['MONGO_USER_PORT']),
+    'connect': False,
 }
 app.secret_key = os.urandom(24)
 
